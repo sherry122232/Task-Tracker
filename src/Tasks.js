297e7f26716c8cff07cfc3ee6cc21c4287d0906a
Askar Bashirov <zep_stump@berkeley.edu>
2023-04-20 17:23:41 -0700

    fixed colors

--- src/Tasks.js
+++ src/Tasks.js
@@ -247,10 +247,23 @@ function TasksItems({ user, filteredTasks, setEditTask }) {
   const formatDate = (date) => {
     return new Date(date).toLocaleDateString();
   };
+
+  // Changes color based on status
+  const determineBackgroundColor = (status) => {
+    if (status === "in-progress") {
+      return "#fff099";
+    }
+    if (status === "to-do") {
+      return "#c2dbf7";
+    }
+    return "#9df0c0";
+  }
+
   return (
     <ul className="tasks__list">
       {filteredTasks.map((task) => (
-        <li className="tasks__list-item" key={task.name}>
+        <li className="tasks__list-item" key={task.name} 
+        style={{backgroundColor: determineBackgroundColor(task.data.status)}}>
           <div className="tasks__item-main">
             <span className="tasks__task-name">{task.data.name}</span>
             <p className="tasks__task-description">{task.data.text}</p>
